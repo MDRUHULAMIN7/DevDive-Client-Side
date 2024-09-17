460ef6e859dec5b7a6318e1c8f25592f1d447f54
Fardus-Hassan <webdev.fardus@gmail.com>
2024-09-17 21:33:48 +0600

    working on navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-
+import { useState } from 'react';
 
 function App() {
-
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <>
-      <h2>New line created by nifat</h2>
-     <div className='text-4xl text-red-300'>dev dive</div>
+      <nav className="relative bg-white shadow hidden sm:hidden lg:block">
+        <div className="px-6 py-3 mx-auto md:flex">
+          <div className="flex items-center justify-between">
+            <a href="#">
+              <img className="w-auto h-6 sm:h-7" src="https://merakiui.com/images/full-logo.svg" alt="Logo" />
+            </a>
+          </div>
+
+          <div>
+
+            
+          </div>
+        </div>
+      </nav>
+
+
     </>
   )
 }
