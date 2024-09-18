edea64a4424970f50435d979d9d96d78f492b469
adnan2508 <adnandiu2800@gmail.com>
2024-09-19 05:07:39 +0600

    resolving conflict

--- src/components/adnan/CreatePost.jsx
+++ src/components/adnan/CreatePost.jsx
@@ -23,7 +23,7 @@ const CreatePost = () => {
         className="block mt-2 w-full placeholder-gray-400/70 rounded-lg border border-gray-200 bg-white px-4 h-32 py-2.5 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40" ></input>
 
       <div className="flex justify-end">
-        <button className="bg-sky-700 px-9 py-3 rounded-lg text-white mt-3">
+        <button className="bg-sky-700 px-8 py-3 rounded-lg text-white mt-3">
           Post
         </button>
       </div>
