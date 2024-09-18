92158de8bdca561131434331c194cb25075fb6a4
Fardus-Hassan <webdev.fardus@gmail.com>
2024-09-19 03:46:27 +0600

    added some route in sidebar and created some pages

--- src/Components/Fardus/Sidebar/Sidebar.jsx
+++ src/Components/Fardus/Sidebar/Sidebar.jsx
@@ -9,25 +9,25 @@ const Sidebar = ({ setOpenMenu, openMenu }) => {
         <aside className={`fixed flex-col w-64 lg:translate-x-0 ${openMenu ? "translate-x-0 duration-200 ease-in-out" : "translate-x-[-256px] duration-200 ease-in-out"} h-[calc(100vh-68px)] px-4 py-8 overflow-y-auto dark:bg-themeColor border-r dark:border-gray-700 rtl:border-r-0 rtl:border-l mt-[1px]`}>
             <div className="flex flex-col justify-between w-[calc(220px-5px)]">
                 <nav>
-                    <NavLink className="flex items-center px-4 py-2 rounded-md duration-200 hover:bg-pm-color hover:text-white" href="#">
+                    <NavLink to='/' className="flex items-center px-4 py-2 rounded-md duration-200 hover:bg-pm-color hover:text-white" href="#">
                         <IoMdHome className='text-lg' />
                         <span className="mx-3 font-medium text-sm">Home</span>
                     </NavLink>
 
-                    <a className="flex items-center px-4 py-2 mt-5 rounded-md duration-200 hover:bg-pm-color hover:text-white" href="#">
+                    <NavLink to='/popular' className="flex items-center px-4 py-2 mt-5 rounded-md duration-200 hover:bg-pm-color hover:text-white" href="#">
                         <FaArrowTrendUp className='text-lg' />
                         <span className="mx-4 font-medium text-sm">Popular</span>
-                    </a>
+                    </NavLink>
 
-                    <a className="flex items-center px-4 py-2 mt-5 duration-200 hover:bg-pm-color hover:text-white rounded-md" href="#">
+                    <NavLink to='/following' className="flex items-center px-4 py-2 mt-5 duration-200 hover:bg-pm-color hover:text-white rounded-md" href="#">
                         <RiUserFollowFill className='text-lg' />
                         <span className="mx-4 font-medium text-sm">Following</span>
-                    </a>
+                    </NavLink>
 
-                    <a className="flex items-center px-4 py-2 mt-5 duration-200 hover:bg-pm-color hover:text-white rounded-md" href="#">
+                    <NavLink to='/all' className="flex items-center px-4 py-2 mt-5 duration-200 hover:bg-pm-color hover:text-white rounded-md" href="#">
                         <MdBarChart className='text-lg' />
                         <span className="mx-4 font-medium text-sm">All</span>
-                    </a>
+                    </NavLink>
 
                     <hr className="my-6 border-gray-200 dark:border-gray-700" />
                 </nav>
