d2081fdd3eacc91ecdcc190c7d5988f85fda9a8a
firoz.nur <firoznur5@gmail.com>
2024-09-20 12:28:27 +0600

    Create : Route For SignModal

--- src/Routeses/Routes.jsx
+++ src/Routeses/Routes.jsx
@@ -7,54 +7,54 @@ import Popular from "../Pages/Popular/Popular";
 import Following from "../Pages/Following/Following";
 import All from "../Pages/All/All";
 import CreatePost from "../components/adnan/CreatePost";
-
 import AdminSetting from "../Pages/Ruhul/Admin/AdminSetting.jsx/AdminSetting";
+import SignModal from "../Components/Nur/SignModal";
 
 export const router = createBrowserRouter([
-    {
-        path:'/',
-        element:<Main></Main>,
-        errorElement:<ErrorPage></ErrorPage>,
-        children:[
-            {
-                path:"/",
-                element:<Home></Home>
-            },
-            {
-                path:"/popular",
-                element:<Popular></Popular>
-            },
-            {
-                path:"/following",
-                element:<Following></Following>
-            },
-            {
-                path:"/all",
-                element:<All></All>
-            },
+  {
+    path: "/",
+    element: <Main></Main>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/popular",
+        element: <Popular></Popular>,
+      },
+      {
+        path: "/following",
+        element: <Following></Following>,
+      },
+      {
+        path: "/all",
+        element: <All></All>,
+      },
 
-            {
-                path:'/card',
-                element:<Card></Card>
-            },
-            {
-                path:'/create-post',
-                element:<CreatePost></CreatePost>
-            },
-            {
-                path:'/create-post',
-                element:<CreatePost></CreatePost>
-            },
+      {
+        path: "/card",
+        element: <Card></Card>,
+      },
+      {
+        path: "/create-post",
+        element: <CreatePost></CreatePost>,
+      },
+      {
+        path: "/create-post",
+        element: <CreatePost></CreatePost>,
+      },
+      {
+        path: "/signModal",
+        element: <SignModal></SignModal>,
+      },
 
-
-            // for admin
-            {
-                path:'/admin-settings',
-                element:<AdminSetting></AdminSetting>
-            },
-            
-       
-            
-        ]
-    }
-])
+      // for admin
+      {
+        path: "/admin-settings",
+        element: <AdminSetting></AdminSetting>,
+      },
+    ],
+  },
+]);
