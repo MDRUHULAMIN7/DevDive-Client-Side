00f8d08245db75693f29528c858caa0be66e4c79
firoz.nur <firoznur5@gmail.com>
2024-10-01 15:33:00 +0600

    Add : UseLeaderBoardPosts Custom Hooks

--- src/Components/Nur/LeaderBoard.jsx
+++ src/Components/Nur/LeaderBoard.jsx
@@ -1,10 +1,17 @@
 import UseUSer from "../../Hooks/UseUser";
+import UseLeaderBoardPosts from "../../Hooks/Nur/UseLeaderBoardPosts";
 
 const LeaderBoard = () => {
   const [users] = UseUSer();
-  console.log(users);
+  const [leaderBoardPosts] = UseLeaderBoardPosts();
+  console.log(leaderBoardPosts);
 
-  return <div>LeaderBoard : Users Count - {users.length}</div>;
+  return (
+    <div>
+      LeaderBoard : Users Count - {users.length} Post :{" "}
+      {leaderBoardPosts.length}
+    </div>
+  );
 };
 
 export default LeaderBoard;
