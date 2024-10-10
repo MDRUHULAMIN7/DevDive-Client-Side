5e04b877d4b1ee354eb1fa3ddc0eca56f86fd81e
Ruhul <ruhulthisis@gmail.com>
2024-10-10 15:25:04 +0600

    follower page update

--- src/Pages/Ruhul/Admin/Component/Followers/Followers.jsx
+++ src/Pages/Ruhul/Admin/Component/Followers/Followers.jsx
@@ -1,18 +1,18 @@
 import { useEffect, useState } from 'react';
-
 import useAxiosPublic from '../../../../../Hooks/useAxiosPublic';
 
 const Followers = () => {
     const [followersData, setFollowersData] = useState([]);
-     const axiosPublic = useAxiosPublic()
- 
+    const axiosPublic = useAxiosPublic();
+
     useEffect(() => {
         const fetchFollowers = async () => {
             try {
                 const response = await axiosPublic.get('/followers/all');
                 console.log('API Response:', response.data);
+
+                // Check if the API response is an array and set the followers data
                 const followers = Array.isArray(response.data) ? response.data : [];
-                console.log(followers);
                 setFollowersData(followers);
             } catch (error) {
                 console.error('Error fetching followers data:', error);
@@ -22,36 +22,48 @@ const Followers = () => {
         fetchFollowers();
     }, [axiosPublic]);
 
-    
-
     return (
         <section className="p-6">
-            <h1 className="text-2xl md:text-3xl lg:text-4xl font-serif font-semibold text-left m-4"> Followers</h1>
+            <h1 className="text-2xl md:text-3xl lg:text-4xl font-serif font-semibold text-left m-4">Followers</h1>
+
             {followersData.length > 0 ? (
                 followersData.map((user, index) => (
                     <div key={index} className="m-4 p-4 border rounded-lg shadow-md">
-                        <h2 className="text-xl font-bold">{user.user}</h2>
-                        <ol className="list-decimal ml-5">
-                        {user.followers
-    .filter((follower, idx, self) =>
-        idx === self.findIndex((t) => (
-            t.email === follower.followersEmail // Change this to a property that uniquely identifies followers
-        ))
-    )
-    .map((follower, idx) => (
-        <li key={idx} className="mt-2 flex items-center">
-    <img 
-        src={follower.followerPhoto} 
-        alt={follower.name} 
-        className="w-10 h-10 rounded-full mr-4" 
-    />
-    <div className="flex flex-col"> 
-        <p className="font-semibold">{follower.name} ({follower.email})</p>
-        <p className="text-sm text-gray-500">Followed on: {follower.followTime}</p>
-    </div>
-</li>
-    ))}
+                        {/* Display the "following" user's image and email */}
+                        <div className="flex items-center mb-4">
+                            <img
+                                src={user.followingPhoto}
+                                alt={user.following}
+                                className="w-12 h-12 rounded-full mr-4"
+                            />
+                            <div>
+                                <h2 className="text-xl font-bold">{user.following}</h2>
+                                <p className="text-sm text-gray-600">{user.followingEmail}</p>
+                            </div>
+                        </div>
 
+                        {/* Show number of followers */}
+                        <p className="text-sm font-medium mb-2">Followers: {user.followers.length}</p>
+
+                        <ol className="list-decimal ml-5">
+                            {/* Map through the followers array for each "following" user */}
+                            {user.followers.map((follower, idx) => (
+                                <li key={idx} className="mt-2 flex items-center">
+                                    <img
+                                        src={follower.followerPhoto}
+                                        alt={follower.followerName}
+                                        className="w-10 h-10 rounded-full mr-4"
+                                    />
+                                    <div className="flex flex-col">
+                                        <p className="font-semibold">
+                                            {follower.followerName} ({follower.followerEmail})
+                                        </p>
+                                        <p className="text-sm text-gray-500">
+                                            Followed on: {follower.followTime}
+                                        </p>
+                                    </div>
+                                </li>
+                            ))}
                         </ol>
                     </div>
                 ))
--- src/Pages/Ruhul/UserProfile/EditProfile.jsx
+++ src/Pages/Ruhul/UserProfile/EditProfile.jsx
@@ -7,12 +7,24 @@ import toast from "react-hot-toast";
 import useAxiosPublic from "../../../Hooks/useAxiosPublic";
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
 import UseAuth from "../../../Hooks/UseAuth";
-import { calcLength } from "framer";
 
 const EditProfile = () => {
   const { email } = useParams();
   const users = useSelector((state) => state.users);
   const dispatch = useDispatch();
+
+  const [name, setName] = useState("");
+  const [email2, setEmail2] = useState("");
+  const [coverImage, setCoverImage] = useState(null);
+  const [profileImage, setProfileImage] = useState(null);
+  const [profilePic, setProfilePic] = useState("");
+  const [coverPic, setCoverPic] = useState("");
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
+  const axiosPublic = useAxiosPublic();
+  const { updateuserprofile } = UseAuth();
+
+  const imageHostApi = import.meta.env.VITE_Cloudinary_API_KEY;
 
   useEffect(() => {
     if (email) {
@@ -20,80 +32,40 @@ const EditProfile = () => {
     }
   }, [dispatch, email]);
 
+  const handleImageUpload = async (file, setImageState) => {
+    if (!file) return;
 
-  const [name, setName] = useState("");
-  const [email2, setEmail2] = useState("");
-  const [coverImage, setCoverImage] = useState(null);
-  const [profileImage, setProfileImage] = useState(null);
-  const [profilePic, setProfilePic] = useState(null);
-  const [coverPic, setCoverPic] = useState(null);
-  const [error, setError] = useState('');
-  const imageHostApi = import.meta.env.VITE_Cloudinary_API_KEY;
-  const [loading, setLoading] = useState(false);
-  const axiosPublic = useAxiosPublic();
- const { updateuserprofile} =UseAuth()
+    const data = new FormData();
+    data.append("file", file);
+    data.append("upload_preset", "bycj1ok9");
+    data.append("cloud_name", "dpomtzref");
 
+    try {
+      const res = await fetch(imageHostApi, {
+        method: "POST",
+        body: data,
+      });
 
-  const handleCoverPhotoChange = async (e) => {
-    const file = e.target.files[0];
-    if (file) {
-      setCoverImage(file);
-      try {
-        const data = new FormData();
-        data.append("file", file);
-        data.append("upload_preset", "bycj1ok9");
-        data.append("cloud_name", "dpomtzref");
+      if (!res.ok) throw new Error("Failed to upload image");
 
-        const res = await fetch(imageHostApi, {
-          method: "POST",
-          body: data,
-        });
-
-        if (!res.ok) {
-          throw new Error("Network response was not ok");
-        }
-
-        const uploadResult = await res.json();
-        const coverPhotoUrl = uploadResult.secure_url;
-        if (coverPhotoUrl) {
-          setCoverPic(coverPhotoUrl);
-        }
-      } catch (error) {
-   
-        setError("Failed to upload cover photo. Please try again.");
-      }
+      const uploadResult = await res.json();
+      const imageUrl = uploadResult.secure_url;
+      setImageState(imageUrl);
+    } catch (error) {
+      setError("Failed to upload image. Please try again.");
     }
   };
 
-  const handleProfilePhotoChange = async (e) => {
+  const handleCoverPhotoChange = (e) => {
     const file = e.target.files[0];
-    if (file) {
-      setProfileImage(file);
-      try {
-        const data = new FormData();
-        data.append("file", file);
-        data.append("upload_preset", "bycj1ok9");
-        data.append("cloud_name", "dpomtzref");
+    setCoverImage(file);
+    handleImageUpload(file, setCoverPic);
+  };
 
-        const res = await fetch(imageHostApi, {
-          method: "POST",
-          body: data,
-        });
-
-        if (!res.ok) {
-          throw new Error("Network response was not ok");
-        }
-
-        const uploadResult = await res.json();
-        const profilePhotoUrl = uploadResult.secure_url;
-        if (profilePhotoUrl) {
-          setProfilePic(profilePhotoUrl);
-        }
-      } catch (error) {
-
-        setError("Failed to upload profile photo. Please try again.");
-      }
-    }
+  const handleProfilePhotoChange = (e) => {
+    const file = e.target.files[0];
+    setProfileImage(file);
+    handleImageUpload(file, setProfilePic);
   };
 
   const handleEditProfile = async (e) => {
@@ -102,139 +74,74 @@ const EditProfile = () => {
     setLoading(true);
 
     if (email2 && email2 !== email) {
-      toast("Please provide your correct User Email!");
+      toast.error("Please provide your correct User Email!");
       setError("Please provide your correct User Email!");
+      setLoading(false);
       return; 
     }
 
-    const updatedUSerInfo={
-      name : name ? name : users?.users?.mainuser?.name,
-      photoUrl : profilePic ?profilePic : users?.users?.mainuser?.photoUrl,
-      coverPhoto : coverPic ? coverPic : users?.users?.mainuser?.coverPic ?  users?.users?.mainuser?.coverPic : '' ,
-    }
+    const updatedUserInfo = {
+      name: name || users?.users?.mainuser?.name,
+      photoUrl: profilePic || users?.users?.mainuser?.photoUrl,
+      coverPhoto: coverPic || users?.users?.mainuser?.coverPhoto || '',
+    };
 
-     if(updatedUSerInfo){
-      console.log(updatedUSerInfo)
-       try {
-        await updateuserprofile(updatedUSerInfo?.name,updatedUSerInfo?.photoUrl);
-        await axiosPublic.put(`/users-update/${email}`, updatedUSerInfo);
-    
-        toast.success("Profile updated successfully!");
-      } catch (error) {
-        toast.error("Failed to update profile. Please try again.");
-        setError('Failed to update profile. Please try again.')
-      
-      } finally {
-        setLoading(false);
-      }
-     }
+    try {
+      await updateuserprofile(updatedUserInfo.name, updatedUserInfo.photoUrl);
+      await axiosPublic.put(`/users-update/${email}`, updatedUserInfo);
+      toast.success("Profile updated successfully!");
+    } catch (error) {
+      toast.error("Failed to update profile. Please try again.");
+      setError('Failed to update profile. Please try again.');
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
     <section className="p-4 mx-auto md:mx-20 lg:mx-44 max-w-7xl">
       <div className="min-h-screen flex items-center justify-center p-6">
         <div className="bg-white dark:bg-gray-800 shadow-lg rounded-lg max-w-4xl w-full p-6 flex flex-col lg:flex-row gap-6">
-          {/* Form */}
           <div className="flex-1">
-            <h2 className="text-2xl font-bold text-gray-900 dark:text-white mb-6">
-              Edit Profile
-            </h2>
+            <h2 className="text-2xl font-bold text-gray-900 dark:text-white mb-6">Edit Profile</h2>
             <form onSubmit={handleEditProfile} className="space-y-8">
               {/* Cover Photo */}
               <div className="relative mb-6">
-                <label
-                  htmlFor="coverPhotoInput"
-                  className="flex items-center justify-center gap-x-2 w-full px-4 py-2 text-sm text-gray-500 dark:text-gray-300 bg-gray-50 dark:bg-gray-700 border-2 border-gray-300 dark:border-gray-600 rounded-md cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-600 transition-all duration-300"
-                >
+                <label htmlFor="coverPhotoInput" className="flex items-center justify-center gap-x-2 w-full px-4 py-2 text-sm text-gray-500 dark:text-gray-300 bg-gray-50 dark:bg-gray-700 border-2 border-gray-300 dark:border-gray-600 rounded-md cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-600 transition-all duration-300">
                   <RiImageAddLine className="text-xl text-blue-500" />
-                  <span className="text-gray-500 dark:text-gray-300">
-                    {coverImage ? "Cover Photo Selected" : "Add Cover"}
-                  </span>
-                  <input
-                    id="coverPhotoInput"
-                    type="file"
-                    accept="image/*"
-                    onChange={(e) => {
-                      handleCoverPhotoChange(e);
-                      handleImageUpload(e, setCoverImage);
-                    }}
-                    className="hidden"
-                  />
+                  <span className="text-gray-500 dark:text-gray-300">{coverImage ? "Cover Photo Selected" : "Add Cover"}</span>
+                  <input id="coverPhotoInput" type="file" accept="image/*" onChange={handleCoverPhotoChange} className="hidden" />
                 </label>
               </div>
 
               {/* Profile Picture */}
               <div className="relative mb-6">
-                <label
-                  htmlFor="profilePicInput"
-                  className="flex items-center justify-center gap-x-2 w-full px-4 py-2 text-sm text-gray-500 dark:text-gray-300 bg-gray-50 dark:bg-gray-700 border-2 border-gray-300 dark:border-gray-600 rounded-md cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-600 transition-all duration-300"
-                >
+                <label htmlFor="profilePicInput" className="flex items-center justify-center gap-x-2 w-full px-4 py-2 text-sm text-gray-500 dark:text-gray-300 bg-gray-50 dark:bg-gray-700 border-2 border-gray-300 dark:border-gray-600 rounded-md cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-600 transition-all duration-300">
                   <RiImageAddLine className="text-xl text-blue-500" />
-                  <span className="text-gray-500 dark:text-gray-300">
-                    {profilePic ? "Profile Picture Selected" : "Add Profile"}
-                  </span>
-                  <input
-                    id="profilePicInput"
-                    type="file"
-                    accept="image/*"
-                    onChange={(e) => {
-                      handleProfilePhotoChange(e);
-                      handleImageUpload(e, setProfileImage);
-                    }}
-                    className="hidden"
-                  />
+                  <span className="text-gray-500 dark:text-gray-300">{profilePic ? "Profile Picture Selected" : "Add Profile"}</span>
+                  <input id="profilePicInput" type="file" accept="image/*" onChange={handleProfilePhotoChange} className="hidden" />
                 </label>
               </div>
 
               {/* Name Input */}
               <div className="relative mb-6">
-                <input
-                  type="text"
-                  id="nameInput"
-                  value={name}
-                  onChange={(e) => setName(e.target.value)}
-                  className="block w-full px-4 py-3 text-sm text-gray-900 dark:text-white bg-transparent border-b-2 border-gray-300 dark:border-gray-600 focus:outline-none focus:ring-0 focus:border-blue-500 transition-all duration-300 peer placeholder-transparent"
-                  placeholder=" "
-                />
-                <label
-                  htmlFor="nameInput"
-                  className={`absolute left-4 top-0 transform transition-all duration-300
-                    ${name ? "translate-y-[-3px] text-blue-500 text-xs" : "top-1/2 -translate-y-1/2 text-gray-500 text-sm"}
-                  `}
-                >
+                <input type="text" id="nameInput" value={name} onChange={(e) => setName(e.target.value)} className="block w-full px-4 py-3 text-sm text-gray-900 dark:text-white bg-transparent border-b-2 border-gray-300 dark:border-gray-600 focus:outline-none focus:ring-0 focus:border-blue-500 transition-all duration-300 peer placeholder-transparent" placeholder=" " />
+                <label htmlFor="nameInput" className={`absolute left-4 top-0 transform transition-all duration-300 ${name ? "translate-y-[-3px] text-blue-500 text-xs" : "top-1/2 -translate-y-1/2 text-gray-500 text-sm"}`}>
                   Name
                 </label>
               </div>
 
               {/* Email Input */}
               <div className="relative mb-6">
-                <input
-                  type="email"
-                  id="emailInput"
-                  value={email2}
-                  onChange={(e) => setEmail2(e.target.value)}
-                  className="block w-full px-4 py-3 text-sm text-gray-900 dark:text-white bg-transparent border-b-2 border-gray-300 dark:border-gray-600 focus:outline-none focus:ring-0 focus:border-blue-500 transition-all duration-300 peer placeholder-transparent"
-                  placeholder=" "
-                  required
-                />
-                <label
-                  htmlFor="emailInput"
-                  className={`absolute left-4 top-0 transform transition-all duration-300
-                    ${email2 ? "translate-y-[-3px] text-blue-500 text-xs" : "top-1/2 -translate-y-1/2 text-gray-500 text-sm"}
-                  `}
-                >
+                <input type="email" id="emailInput" value={email2} onChange={(e) => setEmail2(e.target.value)} className="block w-full px-4 py-3 text-sm text-gray-900 dark:text-white bg-transparent border-b-2 border-gray-300 dark:border-gray-600 focus:outline-none focus:ring-0 focus:border-blue-500 transition-all duration-300 peer placeholder-transparent" placeholder=" " required />
+                <label htmlFor="emailInput" className={`absolute left-4 top-0 transform transition-all duration-300 ${email2 ? "translate-y-[-3px] text-blue-500 text-xs" : "top-1/2 -translate-y-1/2 text-gray-500 text-sm"}`}>
                   Email <span className="text-red-500">*</span>
                 </label>
               </div>
 
               {/* Submit Button */}
-              <button
-                type="submit"
-                className="w-full py-2 px-4 bg-blue-600 text-white font-medium rounded-md hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:focus:ring-blue-800 transition-all duration-300"
-              >
-                {loading ? <AiOutlineLoading3Quarters className=" mx-auto" />
- : "submit"}
-                
+              <button type="submit" className="w-full py-2 px-4 bg-blue-600 text-white font-medium rounded-md hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:focus:ring-blue-800 transition-all duration-300">
+                {loading ? <AiOutlineLoading3Quarters className="animate-spin mx-auto" /> : "Submit"}
               </button>
 
               {error && <p className="text-lg text-center mx-auto my-3 text-red-500">{error}</p>}
@@ -243,32 +150,15 @@ const EditProfile = () => {
 
           {/* Preview Section */}
           <div className="flex-1">
-            <h2 className="text-2xl font-bold text-gray-800 dark:text-white mb-6">
-              Preview
-            </h2>
+            <h2 className="text-2xl font-bold text-gray-800 dark:text-white mb-6">Preview</h2>
             <div className="flex flex-col items-center">
               <div className="w-full h-32 bg-gray-200 rounded-md mb-4 relative overflow-hidden">
-                {coverPic && (
-                  <img
-                    src={coverPic}
-                    alt="Cover Preview"
-                    className="w-full h-full object-cover"
-                  />
-                )}
+                {coverPic && <img src={coverPic} alt="Cover Preview" className="w-full h-full object-cover" />}
               </div>
               <div className="w-28 h-28 rounded-full border border-gray-300 overflow-hidden mb-4">
-                {profilePic && (
-                  <img
-                    src={profilePic}
-                    alt="Profile Preview"
-                    className="w-full h-full object-cover"
-                  />
-                )}
+                {profilePic && <img src={profilePic} alt="Profile Preview" className="w-full h-full object-cover" />}
               </div>
-              <h3 className="text-lg font-semibold text-gray-800 dark:text-white">
-                {name || "Your Name"}
-              </h3>
-              
+              <h3 className="text-lg font-semibold text-gray-800 dark:text-white">{name || "Your Name"}</h3>
             </div>
           </div>
         </div>
