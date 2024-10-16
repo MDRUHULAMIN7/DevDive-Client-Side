1ea69d58c223494d059c495253faf3e4a995828a
NifatHossain <nifathossain1480@gmail.com>
2024-10-16 13:56:04 +0600

    Added confirmation button for delete

--- src/Components/nifat/DeleteCommentButton.jsx
+++ src/Components/nifat/DeleteCommentButton.jsx
@@ -1,5 +1,7 @@
 
+import Swal from 'sweetalert2';
 import { axiosPublic } from '../../Hooks/useAxiosPublic';
+import toast from 'react-hot-toast';
 
 const DeleteCommentButton = ({comment,refetch,replyRefetch,postRefetch}) => {
     const deleteButton = (id) =>{
@@ -23,10 +25,24 @@ const DeleteCommentButton = ({comment,refetch,replyRefetch,postRefetch}) => {
           }});
         }
       }
+      const handleDelete=()=>{
+        Swal.fire({
+          title: "Are you sure?",
+          text: "You won't be able to revert this!",
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#3085d6",
+          cancelButtonColor: "#d33",
+          confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+          if (result.isConfirmed) {
+            deleteButton(comment._id)
+            toast.success('Your comment has been deleted')
+          }
+        });
+      }
     return(
-        
- <button onClick={()=>deleteButton(comment._id)} className="border-2  hover:bg-red-400 dark:hover:bg-gray-800 rounded-md border-slate-200">Delete</button>
-        
+      <button onClick={handleDelete} className="border-2  hover:bg-red-400 dark:hover:bg-gray-800 rounded-md border-slate-200">Delete</button>
     )
 }
 
