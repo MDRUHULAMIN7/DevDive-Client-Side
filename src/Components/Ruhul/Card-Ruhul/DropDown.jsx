3418a069502a39df793d10d5c81aece0c780ab19
firoz.nur <firoznur5@gmail.com>
2024-10-17 15:08:39 +0600

    Add : Checking Archive Status For Each Post

--- src/Components/Ruhul/Card-Ruhul/DropDown.jsx
+++ src/Components/Ruhul/Card-Ruhul/DropDown.jsx
@@ -3,9 +3,18 @@ import { FaRegFileArchive, FaRegFlag } from "react-icons/fa";
 import { handleArchive } from "../../Nur/HandleArchive&Report";
 import { useContext } from "react";
 import { AuthContext } from "../../../Providers/AuthProvider";
+import useCheckArchiveStatus from "../../../Hooks/Nur/useCheckArchiveStatus";
 
 const DropDown = ({ id, isOpen, toggleDropdown, archiveData }) => {
+  console.log("archiveData from dropdown", archiveData);
+  // check archiveData._id === post_id and archivedBy.email === user.email the show already archived
   const { user } = useContext(AuthContext);
+
+  const { archived, isLoading, error } = useCheckArchiveStatus(
+    archiveData?._id,
+    user?.email
+  );
+  console.log("is archived from dropdown", archived, isLoading, error);
 
   return (
     <section>
@@ -19,8 +28,12 @@ const DropDown = ({ id, isOpen, toggleDropdown, archiveData }) => {
           <ul className="m-0 p-0 bg-white dark:bg-themeColor rounded-xl">
             <li
               onClick={() => handleArchive(archiveData, user)}
-              className="px-4 py-2 hover:bg-gray-100 dark:bg-gray-600 dark:hover:text-black cursor-pointer flex items-center gap-1">
-              <FaRegFileArchive /> Archive
+              className={`px-4 py-2 flex items-center gap-1 ${
+                archived
+                  ? "cursor-not-allowed text-gray-400"
+                  : "cursor-pointer hover:bg-gray-100 dark:hover:text-black dark:bg-gray-600"
+              }`}>
+              <FaRegFileArchive /> {archived ? "Archived" : "Archive"}
             </li>
             <li
               onClick={() => console.log("Report Hit")}
