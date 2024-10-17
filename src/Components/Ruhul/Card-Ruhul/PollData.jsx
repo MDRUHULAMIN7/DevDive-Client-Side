10b147d592e6c1063ec544e51dc8bc2144f19e76
Fardus-Hassan <webdev.fardus@gmail.com>
2024-10-18 04:41:45 +0600

    update polling funcnality

--- src/Components/Ruhul/Card-Ruhul/PollData.jsx
+++ src/Components/Ruhul/Card-Ruhul/PollData.jsx
@@ -1,26 +1,32 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import useAxiosPublic from '../../../Hooks/useAxiosPublic';
+import { AuthContext } from '../../../Providers/AuthProvider';
 
 const PollData = ({ pollPost }) => {
+  const { user, setIsModalOpen } = useContext(AuthContext);
   const axiosPublic = useAxiosPublic();
   const [data, setData] = useState(pollPost);
   const [error, setError] = useState(null);
 
-
   const handleVote = async (pollItem) => {
-    try {
-      const response = await axiosPublic.put(`/posts/${pollPost._id}/poll/vote`, {
-        pollItem,
-      });
+    if (user) {
+      try {
+        const response = await axiosPublic.put(`/posts/${pollPost._id}/poll/vote`, {
+          pollItem,
+          email: user?.email,
+        });
 
-      const updatedPost = response.data;
-      setData(updatedPost);
-    } catch (err) {
-      console.error('Error voting:', err);
-      setError('Error casting vote');
+        const updatedPost = response.data;
+        setData(updatedPost);
+      } catch (err) {
+        console.error('Error voting:', err);
+        setError(err.response?.data?.message || 'Error casting vote');
+      }
+    }
+    if(!user){
+      setIsModalOpen(true)
     }
   };
-
 
   if (error) {
     return <p>{error}</p>;
@@ -42,7 +48,7 @@ const PollData = ({ pollPost }) => {
             <div className="flex items-center">
               <div onClick={() => handleVote(poll.item)} className="w-full bg-gray-400 rounded">
                 <div
-                  className="bg-sec-color text-xs font-medium text-white text-center p-1 rounded"
+                  className="bg-sec-color text-xs font-bold text-white text-center p-1 rounded"
                   style={{ width: `${percentage}%` }}
                 >
                   {percentage}%
