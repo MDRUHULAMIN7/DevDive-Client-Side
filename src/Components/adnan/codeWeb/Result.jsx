b83bede61ae1d9cb114bb71ddf7b35c2b3c1b0fb
adnan2508 <adnandiu2800@gmail.com>
2024-10-22 20:55:54 +0600

    done

--- src/Components/adnan/codeWeb/Result.jsx
+++ src/Components/adnan/codeWeb/Result.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 
-const Result = () => {
+const Result = ({srcCode}) => {
   return (
     <div>
       <div className="bg-[#282c34] p-4 shadow mt-4 rounded-lg">
         <h2 className="text-lg font-semibold mb-2 text-white">Result</h2>
         <iframe
           className="w-full h-60 border border-gray-700 rounded-md"
-          srcDoc={""}
+          srcDoc={srcCode}
           title="output"
           sandbox="allow-scripts"
           width="100%"
           height="100%"
-        ></iframe>
+        >
+        </iframe>
       </div>
     </div>
   );
