030fcf79c5922362236f7de7779be3e56a42c084
adnan2508 <adnandiu2800@gmail.com>
2024-10-22 20:41:38 +0600

    build

--- src/Components/adnan/codeWeb/CodeWeb.jsx
+++ src/Components/adnan/codeWeb/CodeWeb.jsx
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react'
+import { useCallback } from "react";
 import Result from './Result';
+import CodeMirror from "@uiw/react-codemirror";
+import { javascript } from "@codemirror/lang-javascript";
+import { html } from "@codemirror/lang-html";
+import { css } from "@codemirror/lang-css";
 
 const CodeWeb = () => {
+  //* create three usestate 
+  const [html_edit, setHtml_Edit] = useState('');
+  const [css_edit, setCss_Edit] = useState('');
+  const [js_edit, setJs_Edit] = useState('');
+
+  //* Html onchange handler 
+  const onChangeHtml = useCallback((value) => {
+    setHtml_Edit(value);
+  }, []);
+
+  //* Css onchange handler 
+  const onChangeCss = useCallback((value) => {
+    setCss_Edit(value);
+  }, []);
+
+  //* JavaScript onchange handler 
+  const onChangeJavaScript = useCallback((value) => {
+    setJs_Edit(value);
+  }, []);
+
+  //* Create Html Document
+  const srcCode = `
+      <body>${html_edit}</body>
+      <style>${css_edit}</style>
+      <script>${js_edit}</script>
+`
+
   return (
     <div>
       {/* main content  */}
