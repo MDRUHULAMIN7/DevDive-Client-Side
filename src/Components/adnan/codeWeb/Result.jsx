8852a3e419b9157d892bc746aaab17d3edb4b175
adnan2508 <adnandiu2800@gmail.com>
2024-10-23 21:10:38 +0600

    done

--- src/Components/adnan/codeWeb/Result.jsx
+++ src/Components/adnan/codeWeb/Result.jsx
@@ -4,7 +4,7 @@ const Result = ({srcCode}) => {
   return (
     <div>
       <div className="bg-[#282c34] p-4 shadow mt-4 rounded-lg">
-        <h2 className="text-lg font-semibold mb-2 text-white">Resultss</h2>
+        <h2 className="text-lg font-semibold mb-2 text-white">Result</h2>
         <iframe
           className="w-full h-60 border border-gray-700 rounded-md"
           srcDoc={srcCode}
