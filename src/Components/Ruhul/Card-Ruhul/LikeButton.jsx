5d86b313cf8cd8082a39317e4c98f0eb81b2ba5c
Ruhul <ruhulthisis@gmail.com>
2024-10-26 18:54:15 +0600

    change role drop down in also small device

--- src/Components/Ruhul/Card-Ruhul/LikeButton.jsx
+++ src/Components/Ruhul/Card-Ruhul/LikeButton.jsx
@@ -1,15 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FaThumbsUp } from "react-icons/fa";
 import './LikeButton.css'; 
 
 const LikeButton = ({ handleLike,  data, isLoading ,likeInfo}) => {
   const [animate, setAnimate] = useState(false);
 // console.log("response from use ruhul like",likeInfo);
+
+const [isRuhulliked,setisRuhulliked] = useState(likeInfo?.isLiked);
+useEffect(()=>{
+  setisRuhulliked(likeInfo?.isLiked)  // updating the state with the fetched data  if any
+ // console.log("isRuhulliked",isRuhulliked)  // checking the state  if any
+},[likeInfo?.isLiked])
+// console.log("isRuhulliked",isRuhulliked)  // checking the state  if any
   const handleClick = () => {
     if (!isLoading) {
 
       setAnimate(true);
- 
+      setisRuhulliked(likeInfo?.isLiked)
       handleLike(data._id);
 
   
@@ -23,7 +30,7 @@ const LikeButton = ({ handleLike,  data, isLoading ,likeInfo}) => {
     <button
       onClick={handleClick}
       className={`flex items-center space-x-1 transition duration-500 transform ${
-        likeInfo?.isLiked ? "text-blue-500" : "text-gray-600"
+        isRuhulliked? "text-blue-500" : "text-gray-600"
       } ${animate ? 'animate-bounce' : ''}`}
       disabled={isLoading}
     >
